2d3f6ece0792cae779e4f5f258376a45328d379b
FernandoCM18 <fernando.c.m@live.com>
2019-10-09 15:16:33 -0700

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,26 @@
 // ================
-// puerto
+// Puerto
 // ================
 process.env.PORT = process.env.PORT || 3000;
+
+// ================
+// Entorno
+// ================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ================
+// Base de datos
+// ================
+let urlDB ;
+
+if ( process.env.NODE_ENV === 'dev' ) {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://fernandocm:FgIcY3ER1pgeNicG@cluster0-d2wn8.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
+
+
+
+
