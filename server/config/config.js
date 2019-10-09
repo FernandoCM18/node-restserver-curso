4d3bc3bc8a0334760829959278a5c5f940d67a61
FernandoCM18 <fernando.c.m@live.com>
2019-10-09 15:40:04 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB ;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://fernandocm:FgIcY3ER1pgeNicG@cluster0-d2wn8.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
